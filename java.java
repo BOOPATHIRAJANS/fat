88c1e4b34472f06315810e46535a6add1f33a4ba
BOOPATHIRAJANS <sboopathi474@gmail.com>
2017-07-21 09:42:04 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,24 @@
+# fat
+import java.util.Scanner;
+
+
+public class Pri {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a,b;
+			System.out.println("Enter the first number");
+			Scanner s=new Scanner(System.in);
+			a=s.nextInt();
+			System.out.println("Enter the second number");
+			b=s.nextInt();
+			
+	System.out.println(a!=b);  // TODO code application logic here
+	    }
+	    
+	
+	}
+
